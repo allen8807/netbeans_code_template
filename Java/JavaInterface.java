64ca2d8693a7ca629e847fbb65e7ae62b8e6156a
Bolu Zhao <bolu.zhao@citrix.com>
2016-08-19 16:43:21 +0800

    add java interface template

---
+++ Java/JavaInterface.java
@@ -0,0 +1,22 @@
+<#assign licenseFirst = "/*">
+<#assign licensePrefix = " * ">
+<#assign licenseLast = " */">
+<#include "${project.licensePath}">
+
+<#if package?? && package != "">
+package ${package};
+
+</#if>
+/**
+ * @FileName ${name}.java
+ * @date ${date}
+ * @time ${time}
+ * @author ${user}
+ * @Description description of the class
+ * @version 1.00 9999/99/99 ${user}
+ * <p> 9.99 9999/99/99 modifier : modify description </p>
+ * @see reference class
+ */
+public interface ${name} {
+
+}
