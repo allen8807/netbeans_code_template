6a75604b89de7e70a1f1b1d36800af8aabfb224f
Bolu Zhao <bolu.zhao@citrix.com>
2016-08-19 16:15:39 +0800

    add java class template

---
+++ Java/JavaClass.java
@@ -0,0 +1,16 @@
+<#assign licenseFirst = "/*">
+<#assign licensePrefix = " * ">
+<#assign licenseLast = " */">
+<#include "${project.licensePath}">
+
+<#if package?? && package != "">
+package ${package};
+
+</#if>
+/**
+ * @File
+ * @author ${user}
+ */
+public class ${name} {
+
+}
