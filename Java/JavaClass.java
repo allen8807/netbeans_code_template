6d77d1f013d04947eed1b1a360d4794d26cb8a85
飞鸿雪泥 <zhaobl880704@gmail.com>
2016-08-19 16:25:36 +0800

    Update JavaClass.java

--- Java/JavaClass.java
+++ Java/JavaClass.java
@@ -8,8 +8,14 @@ package ${package};
 
 </#if>
 /**
- * @File
+ * @FileName ${name}.java
+ * @date ${date}
+ * @time ${time}
  * @author ${user}
+ * @Description description of the class
+ * @version 1.00 9999/99/99 ${user}
+ * <p> 9.99 9999/99/99 modifier : modify description </p>
+ * @see reference class
  */
 public class ${name} {
 
